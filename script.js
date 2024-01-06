fb0815443c50e20c38836a7c289723860bb0f3e4
sabin bhandari <sabinbhandari86@gmail.com>
2024-01-06 21:36:51 +0545

    second

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-var icon= document.querySelector('.container');
-var love=document.querySelector('i');
-var icon2= document.querySelector('.btn i' );
-var lov=document.querySelector('.btn p');
+var icon = document.querySelector('.container');
+var love = document.querySelector('i');
+var icon2 = document.querySelector('.btn i');
 
 
-var flag=0;
+
+var flag = 0;
 
 
-icon.addEventListener('dblclick',function(){
+icon.addEventListener('dblclick', function () {
 
-love.style.transform='translate(-50%,-50%) scale(1)';
-love.style.opacity='0.8'
-love.style.color="red";
-console.log('hey');
-setTimeout(function(){
-    love.style.opacity='0'
-},1000);
+    love.style.transform = 'translate(-50%,-50%) scale(1)';
+    love.style.opacity = '0.8'
+    love.style.color = "red";
+    console.log('hey');
+    setTimeout(function () {
+        love.style.opacity = '0'
+    }, 1000);
 
 });
 
 
-icon2.addEventListener('click',function(){
-    if (flag==0){
-    love.style.transform='translate(-50%,-50%) scale(1)';
-    love.style.opacity='0.8'
-    love.style.color="red";
+icon2.addEventListener('click', function () {
+    if (flag == 0) {
+        love.style.transform = 'translate(-50%,-50%) scale(1)';
+        love.style.opacity = '0.8'
+        love.style.color = "red";
 
-    setTimeout(function(){
-        love.style.opacity='0'
-    },1000);
-    lov.innerHTML="Liked"
+        setTimeout(function () {
+            love.style.opacity = '0'
+        }, 1000);
 
-    flag=1;
-}
-else{
-    lov.innerHTML="Unliked"
-    flag=0;
-}
-    });
-    
+        flag = 1;
+    }
+    else {
+
+        flag = 0;
+    }
+});
+
 
 
 
